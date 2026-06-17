e9757e810080e5f5d7f562437a15ceb37d02a9db
Nagumo <tualex9029@gmail.com>
2026-06-17 00:31:40 -0500

    Add configuration constants for API and links

---
+++ config.js
@@ -0,0 +1,7 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbyvbLz9oi3BFn3jMx-25wGHntHmJEqzMHaOrGqr2LNWlUX3kFzlQWeijRwF3EAgs2fD/exec";
+
+/* 🔥 LINKS OFICIALES */
+
+const DISCORD_URL = "https://discord.gg/TU_INVITACION";
+
+const WHATSAPP_URL = "https://wa.me/51929784766";
